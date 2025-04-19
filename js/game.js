b90b615d8ed907f57fbfefb1d7ab855fd0d96f68
bl4ck0utd4ys <frederico.sanzovo@escola.pr.gov.br>
2025-04-19 17:27:57 +0000

    alterações

--- js/game.js
+++ js/game.js
@@ -20,3 +20,46 @@ function novaPergunta() {
     document.getElementById("resposta").value = "";
     document.getElementById("resultado").innerText = "";
 }
+
+// Gera números aleatórios, maiores no modo desafio
+function gerarNumeros(modo) {
+    let num1 = Math.floor(Math.random() * 10) + 1;
+    let num2 = Math.floor(Math.random() * 10) + 1;
+
+    if (modo === 'desafio') {
+        num1 *= 2;
+        num2 *= 3;
+    }
+
+    return { num1, num2 };
+}
+
+// Escolhe aletoriamente uma operação: +, - ou *
+function escolherOperacao() {
+    const operacoes = ['+', '-','*'];
+    return operacoes[Math.floor(Math.random() * operacoes.lenght)];
+}
+
+// Calcula o resultado com base nos números e operação
+function calcularResposta(n1, n2, op) {
+    switch (op) {
+        case '+': return n1 + n2;
+        case '-': return n1 - n2;
+        case '*': return n1 * n2;
+    }
+}
+
+// Verifica se a resposta do usuário está correta
+function verificarResposta() {
+    const respostaUsuario = parseInt(document.getElementById("resposta").value);
+
+    if (respostaUsuario === respostaCorreta) {
+        document.getElementById("resultado").innerText = "✔ Resposta Correta!";
+        pontuacao += 10;
+        } else {
+        document.getElementById("resultado").innerText = "❌ Errado! A resposta era ${respostaCorreta}";
+        pontuacao -= 5;
+        }
+
+    document.getElementById("pontuacao").innerText = pontuacao;
+}
